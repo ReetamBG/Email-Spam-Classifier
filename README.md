7c12f983d86583565220213106d3d0f7ab300cb9
ReetamBG <r33t4m@gmail.com>
2024-10-31 00:56:08 +0530

    need to do model selection on models other than Naive Bayes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Email-Spam-Classifier
+An Email Spam Classification model that is trained on a dataset of 5000 emails using the Naive Bayes Classifier.
---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import re
+
+
+estimator = pickle.load(open('estimator', 'rb'))
+# text_preprocessor = pickle.load(open('text_preprocessor', 'rb'))    not working for some reason
+vectorizer = pickle.load(open('vectorizer', 'rb'))
+
+
+def preprocess_text(text):
+    text = text.lower()
+    words = nltk.word_tokenize(text)
+    temp = []
+    pattern = r'^[a-zA-Z]+$'
+    for word in words:
+        if word not in stopwords.words('english') and re.search(pattern, word):
+            temp.append(word)
+    stemmer = PorterStemmer()
+    temp = [stemmer.stem(word) for word in temp]
+
+    processed_text = ' '.join(temp)
+    return processed_text
+
+
+st.title('Email Spam Classifier')
+
+text = st.text_area(
+    "Type email to analyze"
+)
+
+if st.button("Analyze"):
+    text_preprocessed = preprocess_text(text)
+    text_vector = vectorizer.transform([text_preprocessed])    # note that vectorizer needs an array of strings
+    prediction = estimator.predict(text_vector)
+    if prediction == 0:
+        st.write('No spam good good')
+    else:
+        st.write('Yajibu alayka an tastaghfirullah!!')
+
